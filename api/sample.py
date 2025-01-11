282d245ffd108e36278618ea752db6b504c93ff1
Porshen Lai <porshenlai@gmail.com>
2025-01-12 00:29:07 +0800

    20250110

--- api/sample.py
+++ api/sample.py
@@ -5,6 +5,10 @@ class H (WebHome.PostHandler) :
 		super().__init__(args)
 		self.COUNT=0
 	async def handle (self, rio) :
+		"""
+		[Request]  curl --json '{"ABC":123}' http://localhost:40780/sample
+		[Response] {"R":"OK","A":{"ABC":123},"C":1}
+		"""
 		# assert "N" in rio.Session, "Violation"
 		# print("path",rio.path)
 		# s, dbn=[], rio.path.group(2)
