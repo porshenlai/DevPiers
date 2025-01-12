f42708c97971148a1ea8bbcd3b5f76db74a6e18f
Porshen Lai <porshenlai@gmail.com>
2025-01-12 12:09:54 +0800

    add python enviroment installation to play.sh

--- PWS/play.sh
+++ PWS/play.sh
@@ -2,11 +2,22 @@
 ROOT=$(realpath $0) && ROOT=${ROOT%/*}
 DOCS=${ROOT}/docs
 PID=${ROOT}/PWS.pid
+
+acquirePython () {
+	test -f ${ROOT}/py/bin/activate || {
+		python3 -m venv ${ROOT}/py
+		. ${ROOT}/py/bin/activate
+		${VIRTUAL_ENV}/bin/python3 -m pip install -r ${ROOT}/requirements.txt
+	}
+	test -z ${VIRTUAL_ENV} && . ${ROOT}/py/bin/activate
+	echo ${VIRTUAL_ENV}/bin/python3
+}
+
 while test "$1"; do
 case "$1" in
 start)
 	if test -f "${ROOT}/config.json"; then
-		python3 ${ROOT}/daemon.py ${ROOT}/config.json &
+		$(acquirePython) ${ROOT}/daemon.py ${ROOT}/config.json &
 		for s in 1 2 3 4 5 6 7 8 9 10; do
 			if test -f "${PID}"; then
 				echo "Daemon running at " $(cat ${PID})
@@ -30,3 +41,5 @@ status)
 esac
 shift
 done
+
+test "${VIRTUAL_ENV}" && deactivate
