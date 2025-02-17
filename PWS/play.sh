82e50613ee121d8590fb0fd2f62cdfdb40fadb61
Porshen Lai <porshenlai@gmail.com>
2025-02-17 22:43:01 +0800

    remove apps directory

--- PWS/play.sh
+++ PWS/play.sh
@@ -2,14 +2,24 @@
 ROOT=$(realpath $0) && ROOT=${ROOT%/*}
 DOCS=${ROOT}/docs
 PID=${ROOT}/PWS.pid
-PY3="NONE"
 
 while test "$1"; do
 case "$1" in
 start)
 	if test -f "${ROOT}/config.json"; then
-		PY3=$(${ROOT}/../apps/python3/prepare.sh|tail -n 1)
-		${PY3} ${ROOT}/daemon.py ${ROOT}/config.json &
+		cd ${ROOT}
+		if ! test -f __pyenv__/bin/activate; then
+			echo "Install python virtual environment"
+			PY3PKG="https://www.python.org/ftp/python/3.12.4/python-3.12.4-embed-amd64.zip"
+			PIPPKG="https://bootstrap.pypa.io/get-pip.py"
+			python3 -m venv __pyenv__
+			if test -f "${ROOT}/requirements.txt"; then
+				. __pyenv__/bin/activate
+				python3 -m pip install -r ${ROOT}/requirements.txt
+				deactivate
+			fi
+		fi
+		__pyenv__/bin/python3 ${ROOT}/daemon.py ${ROOT}/config.json &
 		for s in 1 2 3 4 5 6 7 8 9 10; do
 			if test -f "${PID}"; then
 				echo "Daemon running at " $(cat ${PID})
--- apps/git/prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if test -z "$(which git)"; then
-	echo "sudo apt install git"
-fi
-
-echo $(which git)
