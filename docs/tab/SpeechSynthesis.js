d8d4ea156f768c276821ae657aad14e50776f2cd
Porshen Lai <porshenlai@gmail.com>
2025-03-03 16:49:11 +0800

    20250303

--- docs/tab/SpeechSynthesis.js
+++ docs/tab/SpeechSynthesis.js
@@ -18,6 +18,11 @@ document.currentScript.value=async (root,args)=>{
   		{"I":"12345678","N":"伊惡刪"},
   		{"I":"87654321","N":"霸氣流"}
 	];
+	let listCN=["I","N"];
+
+	function listRedraw () {
+		(root.querySelector('[WidgetTag="std"]'))._gw().set(list);
+	}
 
 	window.speechSynthesis.getVoices();
 	(function (tb) {
@@ -37,4 +42,39 @@ document.currentScript.value=async (root,args)=>{
 			}
 		});
 	}) (root.querySelector('[WidgetTag="std"]'));
+
+	((panel)=>{
+		if (panel.__ClickHandler__) panel.removeEventListener(panel.__ClickHandler__);
+		panel.addEventListener("click",(evt)=>{
+			try {
+				let btn=Piers.DOM(evt.target).find("[func]");
+				console.log(btn.getAttribute("func"));
+				switch (btn.getAttribute("func")) {
+				case "upload":
+					(async (f)=>{
+						f = (await f)[0];
+						f = await f.decode();
+						f = f.replaceAll(/\r/g,'').split("\n");
+						f = f.map((s)=>s.split("\t"));
+						listCN = f.shift();
+						list = f.reduce((a,r)=>{
+							a.push(r.reduce((r,v,i)=>{
+								if (listCN[i]) r[listCN[i]]=v;
+								return r;
+							},{}));
+							return a;
+						},[]);
+						listRedraw();
+					})(Piers.DATA.upload("text/tab-separated-values"));
+					break;
+				case "download":
+					((d)=>{
+						Piers.DATA(d,"text/tab-separated-values").saveAs(((d)=>d.getFullYear()*10000+100*(1+d.getMonth())+d.getDate()+"-"+d.getHours())(new Date())+".tsv");
+					})( (root.querySelector('[WidgetTag="std"]'))._gw().get()
+						.reduce((a,d)=>a.push(listCN.reduce((r,v,i)=>r.push(d[v])&&r,[]).join("\t"))&&a,[listCN.join("\t")]).join("\n") );
+					break;
+				}
+			} catch (x) { console.log(x); }
+		});
+	})(root.querySelector('[UIE="FunctionBar"]'));
 };
